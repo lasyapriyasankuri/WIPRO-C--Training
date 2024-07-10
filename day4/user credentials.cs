3b0d5312243ae1df2c832ca0164b35aa1ff4288e
lasyapriyasankuri <lasyapriyasankuri@gmail.com>
2024-07-10 16:02:50 +0530

    Add files via upload

---
+++ day4/user credentials.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace UserLogin
+{
+    public class UserLogin
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter the User Id: ");
+            string userId = Console.ReadLine();
+            userId = userId.ToLower();
+            Console.WriteLine("enter the password: ");
+            string password = Console.ReadLine();
+            if (userId.Trim() == *admin* & password.Trim () == *admin123*)
+            {
+                Console.WriteLine("Welcome to admin");
+            }
+            else
+            {
+                Console.WriteLine("Invalid User id or Password");
+            }
+
+        }
+    }
+}
