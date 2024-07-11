d16236e13779fdbe81af93839f2c20106f6a1b62
lasyapriyasankuri <lasyapriyasankuri@gmail.com>
2024-07-11 10:51:46 +0530

    Add files via upload

---
+++ day4/triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Triangle
+{
+    static void Main()
+    {
+        int n = 5;
+        for (int i = 1; i <= n; i++)
+        {
+            for( int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
